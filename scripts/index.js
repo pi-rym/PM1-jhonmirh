853616bb27d2531082d0f20d1c60451e0f89a176
jhonmirh <jhonmirh@gmail.com>
2024-03-11 20:23:42 -0400

    Actualizacion errores

---
+++ scripts/index.js
@@ -0,0 +1,25 @@
+class Activity{
+    constructor(id,title,descripcion,imgUrl){
+    this.id = id;
+    this.title = title;
+    this.descripcion = descripcion;
+    this.imgUrl = imgUrl;
+}
+}
+
+class Repository{
+    constructor(){
+        this.activities = [];
+        this.id = 0;
+    }
+
+    getallActivities(){
+        return this.activities;
+}
+
+    createActivity(title,descripcion,imgUrl){
+        const id = this.id++;
+        const activity = new Activity(id,title,descripcion,imgUrl);
+        this.activities.push(activity);
+    }
+}
